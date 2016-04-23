8fe2084f4782a6da2734ba2c8d47a392b4c1f678
Christian Kniep <christian@qnib.org>
2016-04-23 16:07:26 +0200

    add container specific symlink if possible

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -e
 
-if [ -S /shareddev/log ];then
+if [ -S /shareddev/$(hostname)/log ];then
+    ln -s /shareddev/$(hostname)/log /dev/log
+    logger -s "Created container specific symlink to syslog container"
+elif [ -S /shareddev/log ];then
     ln -s /shareddev/log /dev/log
     logger -s "Created symlink from syslog container"
 fi
