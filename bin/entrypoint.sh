9a903ca07812bc1b9daafe4b07c366e17ccae67c
Christian Kniep <christian@qnib.org>
2016-04-24 11:14:10 +0200

    if LOGNAME is NOT empty

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -e
 
-if [ -z ${LOGNAME} ] && [ -S /shareddev/${LOGNAME}/log ];then
+if [ ! -z ${LOGNAME} ] && [ -S /shareddev/${LOGNAME}/log ];then
     ln -s /shareddev/${LOGNAME}/log /dev/log
     logger -s "Created container specific symlink to syslog container"
 elif [ -S /shareddev/log ];then
