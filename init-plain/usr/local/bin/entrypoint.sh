9376ac5cd48179f62fb629b3536b08850ff4fb66
Christian Kniep <ckniep@gaikai.com>
2016-10-20 15:26:57 +0200

    do not use dumb-init

--- init-plain/usr/local/bin/entrypoint.sh
+++ init-plain/usr/local/bin/entrypoint.sh
@@ -1,13 +1,6 @@
-#!/usr/local/bin/dumb-init /bin/bash
+#!/bin/bash
 set -e
 
-if [ ! -z ${LOGNAME} ] && [ -S /shareddev/${LOGNAME}/log ];then
-    ln -s /shareddev/${LOGNAME}/log /dev/log
-    logger -s "Created container specific symlink to syslog container"
-elif [ -S /shareddev/log ];then
-    ln -s /shareddev/log /dev/log
-    logger -s "Created symlink from syslog container"
-fi
 for x in $(find /opt/qnib/entry/ -type f -perm +111 |sort);do
      echo "> execute entrypoint '${x}'"
      "${x}"
