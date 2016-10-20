deeaa326340b7b4b73bff78e7211005fe95a11c0
Christian Kniep <ckniep@gaikai.com>
2016-10-20 14:12:55 +0200

    allow different user to be used at the end of entrypoint

--- init-plain/usr/local/bin/entrypoint.sh
+++ init-plain/usr/local/bin/entrypoint.sh
@@ -13,4 +13,8 @@ for x in $(find /opt/qnib/entry/ -type f -perm +111 |sort);do
      "${x}"
 done
 
-exec su -l /bin/bash -c "$@" user
+if [ "X${ENTRY_USER}" != "X" ];then
+  exec su -s /bin/bash -c "$@" ${ENTRY_USER}
+else
+  exec "$@"
+fi
