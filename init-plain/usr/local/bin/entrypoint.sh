c192d952d8256743f4a70c02eb972505be05db50
Christian Kniep <christian@qnib.org>
2017-04-06 15:54:59 +0200

    skip entrypoints if SKIP_ENTRYPOINT is set

--- init-plain/usr/local/bin/entrypoint.sh
+++ init-plain/usr/local/bin/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-for x in $(find /opt/qnib/entry/ -type f -perm /u+x |sort);do
-     echo "> execute entrypoint '${x}'"
-     if [[ "$x" == *.env ]];then
-         source ${x}
-     else
-         ${x}
-     fi
-done
+if [[ -z ${SKIP_ENTRYPOINTS} ]];then
+    for x in $(find /opt/qnib/entry/ -type f -perm /u+x |sort);do
+        echo "> execute entrypoint '${x}'"
+        if [[ "$x" == *.env ]];then
+            source ${x}
+        else
+            ${x}
+        fi
+   done
+fi
 
 if [ "X${ENTRY_USER}" != "X" ];then
   echo "> execute CMD as user '${ENTRY_USER}'"
