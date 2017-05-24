89fddcfddd941a4601e98174e89e6f4f2dd3ec57
Christian Kniep <christian@qnib.org>
2017-05-24 12:21:11 +0200

    allow ALLOW_HEALTHCHECK_OVERWRITE

--- init-plain/usr/local/bin/healthcheck.sh
+++ init-plain/usr/local/bin/healthcheck.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "[II] qnib/init-plain script v0.4.21"
-HEALTHCHECK_DIR=${HEALTHCHECK_DIR:-/opt/qnib/healthchecks/}
+HEALTHCHECK_DIR=${HEALTHCHECK_DIR:-/opt/healthchecks/}
 set -e 
 
 if [[ ! -d  ${HEALTHCHECK_DIR} ]];then
     echo "> No healthcheck dir found '${HEALTHCHECK_DIR}'"
+    exit 0
+fi
+if [[ "X${ALLOW_HEALTHCHECK_OVERWRITE}" == "Xtrue" ]] && [[ -f ${HEALTHCHECK_DIR}/force_true ]];then
+    echo "> Healthcheck is set to OK via '${HEALTHCHECK_DIR}/force_true' and ALLOW_HEALTHCHECK_OVERWRITE=true"
     exit 0
 fi
 while read -r line;do 
