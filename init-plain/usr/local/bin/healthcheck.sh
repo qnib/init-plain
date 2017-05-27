32711771d4d4e96c6b72f1c2be87ba526a00b45f
Christian Kniep <christian@qnib.org>
2017-05-27 12:15:55 +0200

    allow for initialization phase

--- init-plain/usr/local/bin/healthcheck.sh
+++ init-plain/usr/local/bin/healthcheck.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 echo "[II] qnib/init-plain script v0.4.23"
 HEALTHCHECK_DIR=${HEALTHCHECK_DIR:-/opt/healthchecks/}
-set -e 
+set -e
+ERRORCODES=0
 
 if [[ ! -d  ${HEALTHCHECK_DIR} ]];then
     echo "> No healthcheck dir found '${HEALTHCHECK_DIR}'"
     exit 0
 fi
 if [[ "X${ALLOW_HEALTHCHECK_OVERWRITE}" == "Xtrue" ]] && [[ -f ${HEALTHCHECK_DIR}/force_true ]];then
-    echo "> Healthcheck is set to OK via '${HEALTHCHECK_DIR}/force_true' and ALLOW_HEALTHCHECK_OVERWRITE=true"
-    exit 0
+    echo ">> INITIALIZATION PHASE! Until HC successed the first time, it will be overwritten to be OK."
+    set +e
 fi
-while read -r line;do 
+while read -r line;do
     echo "> execute healthcheck '${x}'"
     ${x}
+    EC=$?
+    if [[ ${EC} -ne 0 ]];then
+        echo ">> Healthcheck failed, but as '${HEALTHCHECK_DIR}/force_true' is set it will be ignored"
+    fi
+    ERRORCODES+=$EC
 done <<< $(find ${HEALTHCHECK_DIR} -type f -perm /u+x |sort)
+if [[ ${ERRORCODES} == 0 ]];then
+    echo "> INITIALIZATION stops, removing ''${HEALTHCHECK_DIR}/force_true'"
+    rm -f ${HEALTHCHECK_DIR}/force_true
+fi
 exit 0
