32ff6aefaa66ac1195f3535d23c407afd03a65bc
Christian Kniep <christian@qnib.org>
2017-05-29 16:31:40 +0200

    check for ENTRYPOINTS_DIR

--- init-plain/usr/local/bin/entrypoint.sh
+++ init-plain/usr/local/bin/entrypoint.sh
@@ -19,14 +19,20 @@ if [[ -z ${SKIP_ENTRYPOINTS} ]];then
             ${x}
         fi
     done
-    for x in $(find ${ENTRYPOINTS_DIR} -type f -perm /u+x |sort);do
-        qecho "> execute entrypoint '${x}'"
-        if [[ "$x" == *.env ]];then
-            source ${x}
-        else
-            ${x}
-        fi
-    done
+    if [[ "X${ENTRYPOINTS_DIR}" != "X" ]];then
+      if [[ -d ${ENTRYPOINTS_DIR} ]];then
+        for x in $(find ${ENTRYPOINTS_DIR} -type f -perm /u+x |sort);do
+          qecho "> execute entrypoint '${x}'"
+          if [[ "$x" == *.env ]];then
+              source ${x}
+          else
+              ${x}
+          fi
+        done
+      else
+        echo "!!> Could not find specified ENTRYPOINTS_DIR '${ENTRYPOINTS_DIR}'"
+      fi
+    fi
 fi
 
 if [[ "${WAIT_TASK_SLOT}" != "X" ]] && $(echo "${WAIT_TASK_SLOT}" |sed -e 's/,/ /g' | grep -q -w "${SWARM_TASK_SLOT}");then
