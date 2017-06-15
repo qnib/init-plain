5e2af8074ebd5432cfb8c7c5280b3604be29ccb8
Christian Kniep <christian@qnib.org>
2017-06-15 11:27:12 +0200

    go back to for loop

--- init-plain/usr/local/bin/entrypoint.sh
+++ init-plain/usr/local/bin/entrypoint.sh
@@ -11,24 +11,24 @@ set -e
 
 if [[ -z ${SKIP_ENTRYPOINTS} ]];then
     ## /opt/entry/
-    while read -r x;do
+    for x in $(find /opt/entry/ -type f -perm /u+x |sort);do
         qecho "> execute entrypoint '${x}'"
         if [[ "$x" == *.env ]];then
             source ${x}
         else
             ${x}
         fi
-    done <<< $(find /opt/entry/ -type f -perm /u+x |sort)
+    done 
     if [[ "X${ENTRYPOINTS_DIR}" != "X" ]];then
       if [[ -d ${ENTRYPOINTS_DIR} ]];then
-        while read -r x;do
+        for x in $(find ${ENTRYPOINTS_DIR} -type f -perm /u+x |sort);do
           qecho "> execute entrypoint '${x}'"
           if [[ "$x" == *.env ]];then
               source ${x}
           else
               ${x}
           fi
-        done <<< $(find ${ENTRYPOINTS_DIR} -type f -perm /u+x |sort)
+        done
       else
         echo "!!> Could not find specified ENTRYPOINTS_DIR '${ENTRYPOINTS_DIR}'"
       fi
