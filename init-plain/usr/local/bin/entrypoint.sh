a533617a5dda57212a71aa4d2d90f8637b88fffc
Christian Kniep <christian@qnib.org>
2025-12-02 16:04:22 +0100

    Bump version: 0.5.1 → 0.5.2

--- init-plain/usr/local/bin/entrypoint.sh
+++ init-plain/usr/local/bin/entrypoint.sh
@@ -7,7 +7,7 @@ function qecho() {
     echo $@
   fi
 }
-qecho "[II] qnib/init-plain script v0.5.1"
+qecho "[II] qnib/init-plain script v0.5.2"
 set -e
 
 if [[ -z ${SKIP_ENTRYPOINTS} ]];then 
--- init-plain/usr/local/bin/healthcheck.sh
+++ init-plain/usr/local/bin/healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "[II] qnib/init-plain script v0.5.1"
+echo "[II] qnib/init-plain script v0.5.2"
 HEALTHCHECK_DIR=${HEALTHCHECK_DIR:-/opt/healthchecks/}
 set -e
 ERRORCODES=0
